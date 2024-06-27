1a713c478fbef810c828fc01ab49b7f0a810381b
Siraaj Khandkar <siraaj@khandkar.net>
2024-06-27 11:55:07 -0400

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    file: PathBuf,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    dbg!(&cli);
+}
