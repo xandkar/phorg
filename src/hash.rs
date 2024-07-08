071a4401f1f3d7ba05763b2ce01d5202162a3f2f
Siraaj Khandkar <siraaj@khandkar.net>
2024-07-08 07:27:14 -0400

    Add sha1 support

--- src/hash.rs
+++ src/hash.rs
@@ -2,6 +2,7 @@ use std::{io, path::Path};
 
 #[derive(Debug, Clone, Copy, clap::ValueEnum)]
 pub enum Hash {
+    Sha1,
     Sha256,
     Md5,
     Crc32,
@@ -16,6 +17,7 @@ impl Default for Hash {
 impl Hash {
     pub fn name(&self) -> &'static str {
         match self {
+            Self::Sha1 => "sha1",
             Self::Sha256 => "sha256",
             Self::Md5 => "md5",
             Self::Crc32 => "crc32",
@@ -24,6 +26,7 @@ impl Hash {
 
     pub fn digest(&self, path: &Path) -> io::Result<String> {
         match self {
+            Hash::Sha1 => digest_sha1(path),
             Hash::Sha256 => digest_sha256(path),
             Hash::Md5 => digest_md5(path),
             Hash::Crc32 => digest_crc32(path),
@@ -33,6 +36,27 @@ impl Hash {
             error
         })
     }
+}
+
+fn digest_sha1<P: AsRef<Path>>(path: P) -> io::Result<String> {
+    use std::io::Read;
+
+    use sha1::{Digest, Sha1};
+
+    let path = path.as_ref();
+    let mut file = std::fs::File::open(path)?;
+    let mut hash = Sha1::default();
+    let mut buff = [0; 1024];
+    loop {
+        let n = file.read(&mut buff)?;
+        if n == 0 {
+            break;
+        }
+        hash.update(&buff[..n]);
+    }
+    let digest = hash.finalize();
+    let hex = format!("{:x}", digest);
+    Ok(hex)
 }
 
 fn digest_sha256<P: AsRef<Path>>(path: P) -> io::Result<String> {
